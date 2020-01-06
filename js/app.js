d151cd6e90eb208e024b1c11d5db78c0cb28b9ee
Daneliz Urena <durena402@gmail.com>
2020-01-06 07:36:34 -0500

    what is this

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ window.onload = function() {
 const items = [];
 
 const addItemToDo= function() {
-  const text = document.getElementById("textbox").value;
+  const text = document.getElementById("textBox").value;
 
   items.push({
     priority: "low",
@@ -16,3 +16,18 @@ const addItemToDo= function() {
 
   renderItems();
 };
+
+const renderItems = function(){
+  const list = document.getElementById("textBox");
+    list.innerHTML = "";
+
+    for (let i = 0; i < items.length; i++) {
+      const li = document.createElement("li");
+
+      const div = document.createId("itemInList");
+      div.innerHTML = items[i].content;
+
+      li.append(div);
+      list.append(li);
+    }
+}
