aa1e8f697206874d9fd185deabbd0d1d52ad9ebb
Daneliz Urena <durena402@gmail.com>
2020-01-07 10:18:32 -0500

    functions for buttons added

--- js/app.js
+++ js/app.js
@@ -6,16 +6,33 @@ window.onload = function() {
   const addItemToDo= function() {
     const text = document.getElementById("textBox").value;
 
-    if (text === '') {
-    alert("You must write something!");
-  }
+    if (!text || text === '') {
+      return;
+    }
+
     items.push({
      priority: "low",
      complete: false,
      content: text
    });
-    renderItems();
+   document.getElementById("textBox").value = "";
+
+  renderItems();
  };
+
+ const prioritize = function() {
+   // find item in array
+   console.log(items[this.id]);
+   if (items[this.id].priority === "low") {
+     items[this.id].priority = "high";
+     // move to top
+   } else {
+     items[this.id].priority = "low";
+     // move to bottom
+   }
+   console.log(items[this.id]);
+ }
+
 
 const renderItems = function (){
   const ul = document.getElementById("ul");
@@ -27,20 +44,27 @@ const renderItems = function (){
           div.id = "itemInList";
 
           const span1 = document.createElement("span");
+          span1.id = i;
           span1.className = "priority";
           span1.innerHTML = "!";
+          span1.onclick = prioritizeItem;
 
           const span2 = document.createElement("span");
+          span2.id = i;
           span2.className = "item";
           span2.innerHTML = items[i].content;
 
           const span3 = document.createElement("span");
+          span3.id = i;
           span3.className = "completed";
           span3.innerHTML = "&#10004";
+          //span3.onclick = completeItem;
 
           const span4 = document.createElement("span");
+          span4.id = i;
           span4.className = "remove";
           span4.innerHTML = "&#10008";
+          //span4.onclick = removeItem;
 
         div.append(span1);
         div.append(span2);
